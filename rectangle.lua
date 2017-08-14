96cdad1bf09781f838580b6f71cc1462b5337f0a
Stivius <maxim11853@gmail.com>
2017-08-14 23:49:05 +0300

    new files

---
+++ rectangle.lua
@@ -0,0 +1,25 @@
+Rectangle = {}
+Rectangle.__index = Rectangle
+
+function Rectangle:create(x, y, width, height, color, key)
+	local rect = {}
+   	setmetatable(rect, Rectangle)
+   	rect.x = x
+   	rect.y = y
+   	rect.width = width
+   	rect.height = height
+   	rect.color = getRgb(color)
+   	rect.key = key
+   	rect.speed = 20
+
+   	return rect
+end
+
+function Rectangle:draw()
+	love.graphics.setColor(self.color.red, self.color.green, self.color.blue)
+    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+end
+
+function Rectangle:move()
+	self.x = self.x + self.speed
+end
